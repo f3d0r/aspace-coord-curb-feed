de1cdec14c95370025279f682f230ffd1fdba415
Fedor Paretsky <me@f3d0r.com>
2018-11-11 16:46:24 -0800

    Lint project

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
+//PACKAGE IMPORTS
 var request = require('request');
-var fs = require('fs');
 var pLimit = require('p-limit');
 var turf = require('@turf/turf');
+var Logger = require('logdna');
+var ip = require('ip');
+var os = require('os');
 
+//CONSTANTS
 const limit = pLimit(5);
-const constants = require('./config')
+const constants = require('./config');
 
+//LOGGING SET UP
+var logger = Logger.setupDefaultLogger(process.env.LOG_DNA_API_KEY, {
+    hostname: os.hostname(),
+    ip: ip.address(),
+    app: process.env.APP_NAME,
+    env: process.env.ENV_NAME,
+    index_meta: true,
+    tags: process.env.APP_NAME + ',' + process.env.ENV_NAME + ',' + os.hostname()
+});
+console.log = function (d) {
+    process.stdout.write(d + '\n');
+    logger.log(d);
+};
+logger.write = function (d) {
+    console.log(d);
+};
+
+//MAIN SCRIPT
 execute();
+
 async function execute() {
+    console.log("URLS #                     : " + constants.COORD_FEATURES.length);
+    console.log("STARTING DOWNLOAD");
     var reqs = [];
     constants.COORD_FEATURES.forEach(function (currentFeature) {
         var coordinates = currentFeature.coordinates;
@@ -19,7 +44,20 @@ async function execute() {
         reqs.push(limit(() => getCurbRules(bbox)));
     });
     var responses = await Promise.all(reqs);
-    console.log(responses[0]);
+    console.log("DONE DOWNLOADING, RECEIVED : " + responses.length);
+    var curbRules = [];
+    responses.forEach(function (currentResponse) {
+        curbRules = curbRules.concat(currentResponse.features);
+    });
+    console.log("---------------------------------");
+    console.log("TOTAL RULES RECEIVED       : " + curbRules.length);
+    var curbIDs = curbRules.map(curb => curb.properties.metadata.curb_id);
+    console.log(curbIDs.length);
+    console.log(hasDuplicates(curbIDs));
+}
+
+function hasDuplicates(array) {
+    return (new Set(array)).size !== array.length;
 }
 
 function getCurbRules(bbox) {
